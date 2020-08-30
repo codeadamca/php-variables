79b342ee160df8d07c13c88fa5c00b6d12e4f6bf
Adam <thomasadam83@hotmail.com>
2020-08-30 15:51:50 -0400

    Added starting PHP file

---
+++ variables.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+    <title>PHP Exercise 1: Links and Variables</title>
+</head>
+<body>
+    
+    <h1>PHP Exercise 1: Links and Variables</h1>
+    
+    <p>Use PHP echo and variables to output the 
+        following link information:</p>
+        
+    <hr>
+    
+    <?php
+    
+    $linkName = 'Codecademy';
+    $linkURL = 'https://www.codecademy.com/';
+    $linkImage = 'https://production.cdmycdn.com/webpack/44e01805165bfde4e6e4322c540abf81.svg';
+    $linkDescription = 'Learn to code interactively, for free.';
+    
+    echo '<h1>'.$linkName.'</h1>';
+
+    ?>
+        
+</body>
+</html>
